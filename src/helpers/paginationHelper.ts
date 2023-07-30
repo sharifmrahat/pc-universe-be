7117fe751a08d2da7cddd3306bbf5319b70cc332
sharifmrahat <sharifmrahat@gmail.com>
2023-07-30 15:32:01 +0600

    default limit updated

--- src/helpers/paginationHelper.ts
+++ src/helpers/paginationHelper.ts
@@ -17,7 +17,7 @@ type IOptionsResult = {
 
 const calculatePagination = (options: IOptions): IOptionsResult => {
   const page = Number(options.page || 1)
-  const limit = Number(options.limit || 20)
+  const limit = Number(options.limit || 100)
   const skip = (page - 1) * limit
 
   const sortBy = options.sortBy || 'createdAt'
